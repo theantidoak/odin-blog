5842d7dd9929ba8314182e31f2e87de3a7877d71
theantidoak <tcdoak@gmail.com>
2024-04-15 13:32:59 +0200

    add token authentication back

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -6,27 +6,24 @@ import { CustomRequest } from '../app';
 export const router = express.Router();
 
 function verifyJWTToken(req: Request, res: Response, next: NextFunction) {
-  // const bearerToken = req.cookies.ob_secure_auth;
-  // if (bearerToken === undefined) {
-  //   res.status(401).json({ success: false, message: `Unauthorized access.` });
-  // } else {
-  //   ( req as Request & { jwtToken: string } ).jwtToken = bearerToken;
-  //   next();
-  // }
-  next();
+  const bearerToken = req.cookies.ob_secure_auth;
+  if (bearerToken === undefined) {
+    res.status(401).json({ success: false, message: 'Unauthorized access' });
+  } else {
+    ( req as Request & { jwtToken: string } ).jwtToken = bearerToken;
+    next();
+  }
 }
 
 function authenticateJWTToken(req: CustomRequest, res: Response, next: NextFunction) {
-  // const jwtToken = req.jwtToken;
-  // if (!jwtToken) return res.status(401).json({ success: false, message: 'No jwt token provided' });
+  const jwtToken = req.jwtToken;
+  if (!jwtToken) return res.status(401).json({ success: false, message: 'No jwt token provided' });
 
-  // jwt.verify(jwtToken, process.env.SECRET as Secret, (err, decoded) => {
-  //   if (err) return res.status(401).json({ success: false, message: 'Unauthorized access' });
-  //   req.user = (decoded as any).user;
-  //   next();
-  // });
-
-  next();
+  jwt.verify(jwtToken, process.env.SECRET as Secret, (err, decoded) => {
+    if (err) return res.status(401).json({ success: false, message: 'Unauthorized access' });
+    req.user = (decoded as any).user;
+    next();
+  });
 }
 
 router.get('/posts', verifyJWTToken, authenticateJWTToken, postController.getPosts);
